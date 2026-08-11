94ac377e881f84b6b88ac24cab94bf9b3e6a3d89
DakshRJain737 <dakshjain737@gmail.com>
2026-08-11 19:41:33 +0530

    feat (socket) : add single threaded server socket which performs one write operation

--- src/main/java/com/httpserver/HttpServer.java
+++ src/main/java/com/httpserver/HttpServer.java
@@ -3,6 +3,13 @@ package com.httpserver;
 import com.httpserver.config.Configuration;
 import com.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class HttpServer {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
@@ -11,5 +18,37 @@ public class HttpServer {
         Configuration configuration = ConfigurationManager.getInstance().getCurrentConfiguration();
 
         System.out.println(configuration.getPort());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(configuration.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            // read
+
+            //write
+            String html = "<html><head><title>Simple Java Server</title></head><body><h1>Using java http server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status line : HTTP_Version RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content length: " + html.getBytes().length + CRLF + // header
+                    CRLF +
+                    html +
+                    CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
